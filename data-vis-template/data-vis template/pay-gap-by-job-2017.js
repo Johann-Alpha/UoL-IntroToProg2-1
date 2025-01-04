1f054859bfe544307551f29dd3890f91a5a927f1
Johann <johannalejandromaldonado@gmail.com>
2025-01-04 04:50:45 +0000

    assignment completed

--- data-vis-template/data-vis template/pay-gap-by-job-2017.js
+++ data-vis-template/data-vis template/pay-gap-by-job-2017.js
@@ -84,7 +84,12 @@ function PayGapByJob2017() {
       // size = numJobs
       ellipse(
         /// ???
+        map(propFemale[i], propFemaleMin, propFemaleMax, 0 + this.pad, width - this.pad),
+        map(payGap[i], payGapMin, payGapMax, height - this.pad, 0 + this.pad),
+        map(numJobs[i], numJobsMin, numJobsMax, this.dotSizeMin, this.dotSizeMax)
+
       );
+
     }
   };
 
